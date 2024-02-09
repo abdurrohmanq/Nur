25e84ef9fd7b8d669dd4eb7e927e1ef4f35b242d
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2024-02-09 15:27:06 +0500

    Queries added in PaymentService

---
+++ Nur.Application/UseCases/Payments/Queries/GetPayments/GetAllPaymentsQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using AutoMapper;
+using Nur.Domain.Entities.Payments;
+using Microsoft.EntityFrameworkCore;
+using Nur.Application.Commons.Interfaces;
+using Nur.Application.UseCases.Payments.DTOs;
+
+namespace Nur.Application.UseCases.Payments.Queries.GetPayments;
+
+public class GetAllPaymentsQuery : IRequest<IEnumerable<PaymentDTO>>
+{ }
+
+public class GetAllPaymentsQueryHandler(IMapper mapper,
+    IRepository<Payment> repository) : IRequestHandler<GetAllPaymentsQuery, IEnumerable<PaymentDTO>>
+{
+    public async Task<IEnumerable<PaymentDTO>> Handle(GetAllPaymentsQuery request, CancellationToken cancellationToken)
+    {
+        var payments = await repository.SelectAll().ToListAsync();
+        var mappedPayments = mapper.Map<IEnumerable<PaymentDTO>>(payments);
+        return mappedPayments;
+    }
+}
---
+++ Nur.Application/UseCases/Payments/Queries/GetPayments/GetPaymentQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using AutoMapper;
+using Nur.Application.Exceptions;
+using Nur.Domain.Entities.Payments;
+using Nur.Application.Commons.Interfaces;
+using Nur.Application.UseCases.Payments.DTOs;
+
+namespace Nur.Application.UseCases.Payments.Queries.GetPayments;
+
+public class GetPaymentQuery : IRequest<PaymentDTO>
+{
+    public GetPaymentQuery(long paymentId) { Id = paymentId; }
+    public long Id { get; set; }
+}
+
+public class GetPaymentQueryHandler(IMapper mapper,
+    IRepository<Payment> repository) : IRequestHandler<GetPaymentQuery, PaymentDTO>
+{
+    public async Task<PaymentDTO> Handle(GetPaymentQuery request, CancellationToken cancellationToken)
+        => mapper.Map<PaymentDTO>(await repository.SelectAsync(u => u.Id.Equals(request.Id))
+            ?? throw new NotFoundException($"This payment is not found with id: {request.Id}"));
+}
