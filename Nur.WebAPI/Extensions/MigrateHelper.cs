5bca0aa721ba32ad6170580c60b9496c9686345c
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2024-03-18 16:07:07 +0500

    First git hub action

---
+++ Nur.WebAPI/Extensions/MigrateHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nur.Infrastructure.Contexts;
+
+namespace Nur.WebAPI.Extensions;
+
+public static class MigrateHelper
+{
+    public static void MigrateDatabase(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        context.Database.Migrate();
+    }
+}
